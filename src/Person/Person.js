57b683d4a55326885dd4a28f2807503386238338
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-04 21:19:44 +0600

    update person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { Children, Component } from "react";
 
-const person = (props) => {
-  return (
-    <>
-      <h1>I'm {props.name} and I am {props.age} years old!</h1>
-      <p>{props.children}</p>
-    </>
-  );
-};
+// const person = (props) => {
+//   return (
+//     <>
+//       <h1>I'm {props.name} and I am {props.age} years old!</h1>
+//       <p>{props.children}</p>
+//     </>
+//   );
+// };
+
+class person extends Component {
+  render() {
+    return (
+       <>
+       <h1>I'm {this.props.name} and I am {this.props.age} years old!</h1>
+       <p>{this.props.children}</p>
+       </>
+    );
+  }
+}
 
 // console.log(Math.ceil(Math.random() * 30));
 
